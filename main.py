116f84985616a64fac1254ec2bfd5d66be5990e8
Kyle Weiland <kyle.weiland@improving.com>
2022-07-11 09:56:29 -0400

    Added zip. Adjusted main to work with lambda. Temp reduced random wait (needs to be addressed).

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import helpers
 import actions
 
 min = 0
-max = 300
+max = 1
 
 #os.system("cls")
 
-today = helpers.get_weekday()
+def lambda_handler(event, context):
+    today = helpers.get_weekday()
 
-if today.postDay:
-    actions.random_wait(min, max)
-    result = actions.post_tweet(today)
+    if today.postDay:
+        actions.random_wait(min, max)
+        result = actions.post_tweet(today)
 
-else:
-    result = f"Today is {today.dayStr}. Therefore, Not Hotdog"
+    else:
+        result = f"Today is {today.dayStr}. Therefore, Not Hotdog"
 
-print(result)
+    print(result)
